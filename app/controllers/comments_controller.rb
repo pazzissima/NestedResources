244b3fdeb2af375d427818bb70809764f6280bc4
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2014-02-12 19:25:33 -0800

    this is difficult

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,18 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
     @comment_comments = @comment.comments
   end
+
+  def index
+    @comments = current_user.comments
+  end
+
+  def new
+    @comment = Comment.new 
+  end
+
+  def create
+    comment_params = params.require(:comment).permit(:body, comments_attributes: [ :body ])
+    comment = current_user.comments.create(comment_params)
+    redirect_to comment_path(comment.id)
+  end
 end
